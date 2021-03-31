bd948c2932b89151b590e87ad2265edcb91e7f4c
서시언 <seosieon@seosieon-ui-MacBookPro.local>
2021-03-31 23:20:51 +0900

    week1

---
+++ web/main.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    console.log(queryData.id);
+    if(_url == '/'){
+      _url = '/index.html';
+    }
+    if(_url == '/favicon.ico'){
+      response.writeHead(404);
+      response.end();
+      return;
+    }
+    response.writeHead(200);
+    //response.end(fs.readFileSync(__dirname + _url));
+    response.end(queryData.id);
+
+});
+//portNum = 3000
+app.listen(3000);
